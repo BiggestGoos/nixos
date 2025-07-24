76b5ddf5791f2778109c005e9ea4e06c70a7af58
BiggestGoos <gustav@fagerlind.net>
2025-07-24 15:26:29 +0200

    Fixed locale

--- hosts/vizima/configuration.nix
+++ hosts/vizima/configuration.nix
@@ -19,24 +19,7 @@
       #"${inputs.self}/steam.nix"
 #      (my.utils.fromRoot "steam/steam.nix")
     ];
-
-  #nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Set your time zone.
-  time.timeZone = "Europe/Stockholm";
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  console = {
-  #   font = "Lat2-Terminus16";
-    keyMap = "sv-latin1";
-  #   useXkbConfig = true; # use xkb.options in tty.
-  };
-
+ 
   # Enable the X11 windowing system.
   # services.xserver.enable = true;
 
---
+++ hosts/vizima/system/locale.nix
@@ -0,0 +1,12 @@
+{ ... }:
+{
+
+	time.timeZone = "Europe/Stockholm";
+	
+	i18n.defaultLocale = "en_US.UTF-8";
+
+	console = {
+		keyMap = "sv-latin1";
+	};
+
+}
