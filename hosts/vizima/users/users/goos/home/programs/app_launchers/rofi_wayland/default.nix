3217175ec37cff619e8a5275f942cf64fd1ed84c
BiggestGoos <gustav@fagerlind.net>
2025-07-24 21:58:32 +0200

    Moved rofi config into nixos

--- hosts/vizima/users/users/goos/home/programs/app_launchers/rofi_wayland/default.nix
+++ hosts/vizima/users/users/goos/home/programs/app_launchers/rofi_wayland/default.nix
@@ -1,4 +1,8 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
+let
+	configDir = "rofi";
+	configHomeDir = "${config.xdg.configHome}/${configDir}";
+in
 {
 
 	programs.rofi = {
@@ -7,5 +11,8 @@
 		enable = true;
 
 	};
+
+	#home.file."${configHomeDir}/config.rasi" = ./config.rasi;
+	home.file."${configHomeDir}/current.rasi" = ./current.rasi;
 
 }
