028cb083df78176a5c0dac270f455f1347dcc76a
BiggestGoos <gustav@fagerlind.net>
2025-07-24 14:05:43 +0200

    Made hyprpolkitagent get automatically enabled

--- hosts/vizima/users/desktops/hyprland/polkit.nix
+++ hosts/vizima/users/desktops/hyprland/polkit.nix
@@ -5,4 +5,15 @@
 		pkgs.hyprpolkitagent
 	];
 	
+	# Couldn't get it to work
+	/*systemd.packages = [
+		pkgs.hyprpolkitagent
+	];
+
+	systemd.user.services."hyprpolkitagent".enable = true;*/
+
+	system.userActivationScripts = { 
+		enable_hyprpolkitagent = "${pkgs.systemd}/bin/systemctl --user enable hyprpolkitagent";
+	};
+
 }
