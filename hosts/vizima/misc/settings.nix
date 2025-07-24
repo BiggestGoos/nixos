fc14d1bbc8c2102eda02a33f55de1740120864ae
BiggestGoos <gustav@fagerlind.net>
2025-07-24 10:03:18 +0200

    Look in file for reasons

--- hosts/vizima/misc/settings.nix
+++ hosts/vizima/misc/settings.nix
@@ -5,5 +5,9 @@
 
 	nixpkgs.config.allowUnfree = true;
 
+	# Needed to make certain changes with home-manager not require it to download files after every garbage-collection
+	nix.settings.keep-outputs = true;
+	nix.settings.keep-derivations = true;
+
 }
 
