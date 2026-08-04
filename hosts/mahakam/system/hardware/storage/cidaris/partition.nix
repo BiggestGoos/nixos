dbeef08093ef4b486f85cfedacd5ac2b5d471a4c
BiggestGoos <gustav@fagerlind.net>
2026-08-04 15:50:45 +0200

    mahakam: Add cidaris partitioning.

---
+++ hosts/mahakam/system/hardware/storage/cidaris/partition.nix
@@ -0,0 +1,23 @@
+{
+	disko.devices.disk.cidaris = 
+	{
+		type = "disk";
+		device = "/dev/disk/by-id/ata-ST2000DM008-2UB102_ZFL8ZMJK";
+		content = 
+		{
+			type = "gpt";
+			partitions = 
+			{
+				root =
+				{
+					size = "100%";
+					content =
+					{
+						type = "btrfs";
+						subvolumes."@root" = { };
+					};
+				};
+			};
+		};
+	};
+}
