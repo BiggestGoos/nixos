0e9f799cb0493efdba786a5dbd017c7cc29bc9ec
BiggestGoos <gustav@fagerlind.net>
2026-09-21 10:32:16 +0200

    vizima: Add nixvim.

---
+++ modules/system/misc/printing.nix
@@ -0,0 +1,15 @@
+{ pkgs, ... }:
+{
+  services.printing.enable = true;
+  services.printing.drivers = [ pkgs.hplip ];
+
+  environment.systemPackages = [
+    pkgs.system-config-printer
+  ];
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+}
